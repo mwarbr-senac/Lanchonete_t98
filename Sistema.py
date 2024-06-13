f000baf492fe6d655992a7f74faec5b67d900fab
Maycon Guerra <mwar.library@gmail.com>
2024-06-13 08:09:47 -0300

    Aula de 11/06/24

---
+++ Sistema.py
@@ -0,0 +1,12 @@
+"""
+    Este arquivo é a entrada do sistema, ele aciona os módulos necessários.
+"""
+# importação do Módulo Usuario e da classe
+from Modulos.Usuario import Usuario
+
+usuario = Usuario()
+
+
+
+
+
